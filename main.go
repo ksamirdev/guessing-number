272ff2bfdaf555e98ee96abc72ebf762c54b35a4
Samir <samirdiff@proton.me>
2023-09-01 13:30:27 +0530

    Fixed new line input

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	var s string
 	fmt.Printf("Pick an integer from 0 to 100.\n")
 	answer := sort.Search(100, func(i int) bool {
-		fmt.Printf("Is your number <= %d? If yes then type y", i)
+		fmt.Printf("Is your number <= %d? If yes then type y\n", i)
 		fmt.Scanf("%s", &s)
 		return s != "" && s[0] == 'y'
 	})
